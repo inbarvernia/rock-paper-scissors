14c460fd8cf719dd73816a1728b149a159836a14
loushark <lou.m.reade@gmail.com>
2021-02-23 10:54:38 +0000

    passed test for its a draw

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -2,5 +2,6 @@ def rock_paper_scissors(player1, player2)
   case player2
     when "scissors" then "Player 1 wins"
     when "paper" then "Player 2 wins"
+    when "rock" then "It's a draw"
   end
 end
