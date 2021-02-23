5af59cf8095ebb0441c22fb063a22ede14372dac
Inbar Vernia <inbar.vernia@gmail.com>
2021-02-23 11:04:36 +0000

    Added test for (paper, scissors)

--- spec/rps_spec.rb
+++ spec/rps_spec.rb
@@ -17,4 +17,7 @@ describe "rock_paper_scissors" do
     expect(rock_paper_scissors("paper", "rock")).to eq "Player 1 wins"
   end
   
+  it "Player 2 wins if plays scissors against paper" do
+    expect(rock_paper_scissors("paper", "scissors")).to eq "Player 2 wins"
+  end
 end
