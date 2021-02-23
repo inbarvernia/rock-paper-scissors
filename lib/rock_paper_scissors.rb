e16c0a7b11cc6555d3e4c78600f908b8b26fb09f
loushark <lou.m.reade@gmail.com>
2021-02-23 11:20:37 +0000

    passed test for draw with paper

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -9,6 +9,7 @@ def rock_paper_scissors(player1, player2)
       case player1
         when "rock" then "Player 2 wins"
         when "scissors" then "Player 1 wins"
+        when "paper" then "It's a draw"
       end
     when "rock"
       case player1
