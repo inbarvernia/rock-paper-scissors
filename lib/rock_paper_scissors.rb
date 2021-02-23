a55ff3af724859875e143584dd14c401dea22f36
loushark <lou.m.reade@gmail.com>
2021-02-23 11:08:29 +0000

    passed test for player 2 wins with paper

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -1,6 +1,10 @@
 def rock_paper_scissors(player1, player2)
   case player2
-    when "scissors" then "Player 1 wins"
+    when "scissors"
+      case player1
+        when "paper" then "Player 2 wins"
+        when "rock" then "Player 1 wins"
+      end
     when "paper" then "Player 2 wins"
     when "rock"
       case player1
