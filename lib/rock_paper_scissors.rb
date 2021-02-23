6145a61ac18ec994b77021d4b2c07384b05ce3cb
Inbar Vernia <inbar.vernia@gmail.com>
2021-02-23 11:03:00 +0000

    Passed test for (paper, rock)

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -2,6 +2,10 @@ def rock_paper_scissors(player1, player2)
   case player2
     when "scissors" then "Player 1 wins"
     when "paper" then "Player 2 wins"
-    when "rock" then "It's a draw"
+    when "rock"
+      case player1
+        when "rock" then "It's a draw"
+        when "paper" then "Player 1 wins"
+      end
   end
 end
