cf32ec55c0328a0df73e0e72087500ff1b18f303
loushark <lou.m.reade@gmail.com>
2021-02-23 10:46:41 +0000

    added test for player 2 wins

--- spec/rps_spec.rb
+++ spec/rps_spec.rb
@@ -1,5 +1,11 @@
+require 'rock_paper_scissors'
+
 describe "rock_paper_scissors" do
   it "player 1 is equal to use" do
    expect(rock_paper_scissors("rock", "scissors")).to eq "Player 1 wins"
+  end
+
+  it "player 2 wins if plays paper against rock" do
+    expect(rock_paper_scissors("rock", "paper")).to eq "Player 2 wins"
   end
 end
